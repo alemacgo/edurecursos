b5c2397a44006c638f7f3c992f512899afd47c76
Alejandro Machado <alemacgo@gmail.com>
2015-05-05 09:47:14 -0430

    Start data formatting using Handlebar templates

--- js/get-contents.js
+++ js/get-contents.js
@@ -1,4 +1,11 @@
-// Load an entire worksheet.
-$('#statistics').sheetrock({
-  url: Url
+// Compile the Handlebars template for HR leaders.
+console.log($('#hr-template').html());
+var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+// Load top five HR leaders.
+$('#hr').sheetrock({
+  url: spreadsheetUrl,
+  query: "select A,B,C",
+  fetchSize: 5,
+  rowTemplate: HRTemplate
 });
