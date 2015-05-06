bc7d632a1441bf968b348b1218075ca4f9affb73
Alejandro Machado <alemacgo@gmail.com>
2015-05-06 11:14:15 -0430

    Use a grid-based layout

--- createUnit.py
+++ createUnit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Tkinter import Tk, RIGHT, Frame, Button, BOTH, RAISED
-from ttk import Style
+from Tkinter import *
+#Tk, Frame, Label, Text, Button, BOTH, RIGHT, RAISED, W, N, E, S
 
-class Example(Frame):
+class App(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
@@ -12,16 +12,32 @@ class Example(Frame):
 
     def initUI(self):
         self.parent.title("Crear unidad...")
-        self.style = Style()
-        self.style.theme_use("default")
-
-        frame = Frame(self, relief=RAISED, borderwidth=1)
-        frame.pack(fill=BOTH, expand=1)
-
         self.pack(fill=BOTH, expand=1)
 
-        closeButton = Button(self, text="Crear", command=self.quit)
-        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        Label(self, text="Enlace a la hoja de cálculo").grid(row=0, sticky=W)
+        Label(self, text="Nombre de la unidad").grid(row=1, sticky=W)
+        e1 = Entry(self, width=70)
+        e1.insert(0, "https://docs.google.com/spreadsheets/...")
+        e2 = Entry(self, width=70)
+        e1.grid(row=0, column=1)
+        e2.grid(row=1, column=1)
+
+        abtn = Button(self, text="Crear", command=self.quit)
+        abtn.grid(row=2, column=1, sticky=E)
+
+   # def initUI(self):
+   #     self.parent.title("Crear unidad...")
+   #     self.style = Style()
+   #     self.style.theme_use("default")
+
+   #     frame = Frame(self, relief=RAISED, borderwidth=1)
+   #     frame.pack(fill=BOTH, expand=1)
+
+   #     self.pack(fill=BOTH, expand=1)
+
+   #     closeButton = Button(self, text="Crear", command=self.quit)
+   #     closeButton.pack(side=RIGHT, padx=5, pady=5)
+
         #okButton = Button(self, text="OK")
         #okButton.pack(side=RIGHT)
 
@@ -36,7 +52,7 @@ class Example(Frame):
 def main():
     root = Tk()
     root.geometry("700x150+300+300")
-    app = Example(root)
+    app = App(root)
     root.mainloop()
 
 if __name__ == '__main__':
