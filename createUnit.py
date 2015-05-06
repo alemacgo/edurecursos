e0a08abec57658fd66b90f217343390b53c1d3f4
Alejandro Machado <alemacgo@gmail.com>
2015-05-06 10:22:57 -0430

    Improve layout

--- createUnit.py
+++ createUnit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import Tk, RIGHT, Frame, Button, BOTH, RAISED
+from ttk import Style
 
 class Example(Frame):
     def __init__(self, parent):
@@ -11,7 +12,18 @@ class Example(Frame):
 
     def initUI(self):
         self.parent.title("Crear unidad...")
+        self.style = Style()
+        self.style.theme_use("default")
+
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill=BOTH, expand=1)
+
         self.pack(fill=BOTH, expand=1)
+
+        closeButton = Button(self, text="Crear", command=self.quit)
+        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        #okButton = Button(self, text="OK")
+        #okButton.pack(side=RIGHT)
 
     def centerWindow(self):
         width = 700
