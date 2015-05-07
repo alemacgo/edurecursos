df992cb645b0359684e12b91f09291a5fa63be9d
Alejandro Machado <alemacgo@gmail.com>
2015-05-07 09:00:14 -0430

    Attempt to load the Google spreadsheet name using JQuery

--- js/get-contents.js
+++ js/get-contents.js
@@ -4,12 +4,28 @@ Handlebars.registerHelper('link', function(number) {
   return new Handlebars.SafeString(result);
 });
 
-// Compile the Handlebars template for HR leaders.
+// Compile the Handlebars template
 var HRTemplate = Handlebars.compile($('#hr-template').html());
 
-// Load top five HR leaders.
+// Load data from the spreadsheet
 $('#hr').sheetrock({
   url: spreadsheetUrl,
   query: "select A,B,C",
   rowTemplate: HRTemplate
 });
+
+$(document).ready(loadSpreadsheetName);
+function loadSpreadsheetName() {
+      $.getJSON(spreadsheetUrl, function(data) {
+         if (data && data != null && typeof data == "object" && data.contents && data.contents != null && typeof data.contents == "string") {
+            if (data.length > 0) {
+               if (ContentLocationInDOM && ContentLocationInDOM != null && ContentLocationInDOM != "null") {
+                  $("#unit-name").html($("#docs-title-inner", data));
+               }
+               else {
+                  $("#unit-name").html(data);
+               }
+            }
+         }
+      });
+}
